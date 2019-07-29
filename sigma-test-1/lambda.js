955e2eff1407a665228192ae4fd1dd7d2e82cf5f
lpssudarshana <id.etisalat@gmail.com>
2019-07-29 17:27:32 +0530

    second commit

--- sigma-test-1/lambda.js
+++ sigma-test-1/lambda.js
@@ -1,4 +1,18 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+    ddb.put({
+        TableName: 'sudarshana-test-table',
+        Item: { 'id': '0', 'name': 'sudarshana', 'age': 0 }
+    }).promise()
+        .then((data) => {
+            //your logic goes here
+        })
+        .catch((err) => {
+            //handle error
+        });
+
+callback(null, { "message": "Successfully executed" });
+
 }
