d9a062b5b9a06b6dd8c8edd7e61fb4f6baafc8ff
lpssudarshana <id.etisalat@gmail.com>
2019-07-30 10:32:26 +0530

    added event data binding

--- sigma-test-1/lambda.js
+++ sigma-test-1/lambda.js
@@ -2,9 +2,10 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
+    debugger;
     ddb.put({
         TableName: 'sudarshana-test-table',
-        Item: { 'id': '0', 'name': 'sudarshana', 'age': 0 }
+        Item: {  'name': event.name, 'age': event.age }
     }).promise()
         .then((data) => {
             //your logic goes here
@@ -13,6 +14,6 @@ exports.handler = function (event, context, callback) {
             //handle error
         });
 
-callback(null, { "message": "Successfully executed" });
+    callback(null, { "message": "Successfully executed" });
 
 }
