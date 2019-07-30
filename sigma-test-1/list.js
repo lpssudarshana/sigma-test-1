3a98b54aefb0d5acf102674bfe3552d3e8d7fda7
lpssudarshana <id.etisalat@gmail.com>
2019-07-30 12:23:40 +0530

    change the files

--- sigma-test-1/list.js
+++ sigma-test-1/list.js
@@ -2,11 +2,12 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
+    debugger;
     ddb.scan({
         TableName: 'sudarshana-test-table'
     }).promise()
         .then((data) => {
-            callback(null, { "data": data });
+            callback(null, { "data": data.Items });
         })
         .catch((err) => {
             callback(null, { "error": err });
